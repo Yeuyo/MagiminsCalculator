afa7feff8e7be5bc18bd6221a8fde0b344a053d7
Yew Yan Wong <38599824+Yeuyo@users.noreply.github.com>
2022-11-04 14:15:40 +1100

    Add files via upload
    Added the ability to filter by traits.

--- Potionomics.py
+++ Potionomics.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QEvent
 from PyQt5.QtWidgets import (QAction, QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QDial, QDialog, QGridLayout, QHBoxLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy, QTableWidgetItem,
@@ -76,6 +76,123 @@ class HeaderView(QtWidgets.QHeaderView):
         if li == self.checkable_column:
             self.state = not self.state
 
+class CheckableComboBox(QComboBox):
+    # Yoann Quenach de Quivillic on https://gis.stackexchange.com/questions/350148/qcombobox-multiple-selection-pyqt5
+    # Subclass Delegate to increase item height
+    class Delegate(QStyledItemDelegate):
+        def sizeHint(self, option, index):
+            size = super().sizeHint(option, index)
+            size.setHeight(20)
+            return size
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Make the combo editable to set a custom text, but readonly
+        self.setEditable(True)
+        self.lineEdit().setReadOnly(True)
+        # Make the lineedit the same color as QPushButton
+        # palette = qApp.palette()
+        # palette.setBrush(QPalette.Base, palette.button())
+        # self.lineEdit().setPalette(palette)
+
+        # Use custom delegate
+        self.setItemDelegate(CheckableComboBox.Delegate())
+
+        # Update the text when an item is toggled
+        self.model().dataChanged.connect(self.updateText)
+
+        # Hide and show popup when clicking the line edit
+        self.lineEdit().installEventFilter(self)
+        self.closeOnLineEditClick = False
+
+        # Prevent popup from closing when clicking on an item
+        self.view().viewport().installEventFilter(self)
+
+    def resizeEvent(self, event):
+        # Recompute text to elide as needed
+        self.updateText()
+        super().resizeEvent(event)
+
+    def eventFilter(self, object, event):
+
+        if object == self.lineEdit():
+            if event.type() == QEvent.MouseButtonRelease:
+                if self.closeOnLineEditClick:
+                    self.hidePopup()
+                else:
+                    self.showPopup()
+                return True
+            return False
+
+        if object == self.view().viewport():
+            if event.type() == QEvent.MouseButtonRelease:
+                index = self.view().indexAt(event.pos())
+                item = self.model().item(index.row())
+
+                if item.checkState() == Qt.Checked:
+                    item.setCheckState(Qt.Unchecked)
+                else:
+                    item.setCheckState(Qt.Checked)
+                return True
+        return False
+
+    def showPopup(self):
+        super().showPopup()
+        # When the popup is displayed, a click on the lineedit should close it
+        self.closeOnLineEditClick = True
+
+    def hidePopup(self):
+        super().hidePopup()
+        # Used to prevent immediate reopening when clicking on the lineEdit
+        self.startTimer(100)
+        # Refresh the display text when closing
+        self.updateText()
+
+    def timerEvent(self, event):
+        # After timeout, kill timer, and reenable click on line edit
+        self.killTimer(event.timerId())
+        self.closeOnLineEditClick = False
+
+    def updateText(self):
+        texts = []
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).checkState() == Qt.Checked:
+                texts.append(self.model().item(i).text())
+        text = ", ".join(texts)
+
+        # Compute elided text (with "...")
+        metrics = QtGui.QFontMetrics(self.lineEdit().font())
+        elidedText = metrics.elidedText(text, Qt.ElideRight, self.lineEdit().width())
+        self.lineEdit().setText(elidedText)
+
+    def addItem(self, text, data=None):
+        item = QtGui.QStandardItem()
+        item.setText(text)
+        if data is None:
+            item.setData(text)
+        else:
+            item.setData(data)
+        item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsUserCheckable)
+        item.setData(Qt.Unchecked, Qt.CheckStateRole)
+        self.model().appendRow(item)
+
+    def addItems(self, texts, datalist=None):
+        for i, text in enumerate(texts):
+            try:
+                data = datalist[i]
+            except (TypeError, IndexError):
+                data = None
+            self.addItem(text, data)
+
+    def currentData(self):
+        # Return the list of selected items data
+        res = []
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).checkState() == Qt.Checked:
+                res.append(self.model().item(i).data())
+        return res
+
 class Controller:
     def __init__(self, model, view):
         self._model = model
@@ -91,7 +208,7 @@ class Controller:
     
     def calculateMagimins(self):
         self._view.solutionTableData = QtGui.QStandardItemModel(0, 2, self._view)
-        self._view.solutionTableData, totalMagimins = self._model.getBestCombination(self._view.ingredientTableData, self._view.solutionTableData, self._view.ingredientNumber.value(), self._view.magiminsNumber.value(), self._view.potionMaking.currentText(), self._view.dailyIngredientLimit.currentText())
+        self._view.solutionTableData, totalMagimins = self._model.getBestCombination(self._view.ingredientTableData, self._view.solutionTableData, self._view.ingredientNumber.value(), self._view.magiminsNumber.value(), self._view.potionMaking.currentText(), self._view.dailyIngredientLimit.currentText(), self._view.traitSelection.currentData())
         self._view.solutionTable.setModel(self._view.solutionTableData)
         self._view.solutionTable.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
         self._view.solutionTable.horizontalHeader().setStretchLastSection(True)
@@ -129,7 +246,7 @@ class Model:
             ingredientTableData.appendRow([it_state, it_name, it_a, it_b, it_c, it_d, it_e, it_f])
         return ingredientTableData       
 
-    def getBestCombination(self, ingredientTableData, solutionTableData, ingredientNumber, magiminsNumber, potionMaking, dailyIngredientLimit):
+    def getBestCombination(self, ingredientTableData, solutionTableData, ingredientNumber, magiminsNumber, potionMaking, dailyIngredientLimit, traitSelection):
         states = np.zeros((ingredientTableData.rowCount(), 1))
         for i in range(ingredientTableData.rowCount()):
             states[i] = ingredientTableData.item(i, 0).checkState()
@@ -202,10 +319,46 @@ class Model:
         for n in range(len(magiminUsed)):
             fullList.remove(magiminUsed[n])
         choosenIngredients = usefulIngredients.loc[(usefulIngredients.iloc[:, fullList] == 0).all(axis=1), :]
+        choosenIngredients.reset_index(drop = True, inplace = True)
+        # Select Traits
+        if len(traitSelection) > 0:
+            for n in range(len(traitSelection)):
+                choosenIngredients = choosenIngredients.drop(choosenIngredients.loc[choosenIngredients[traitSelection[n]] < 0,].index)
+            choosenIngredients.reset_index(drop = True, inplace = True)
+            traitIngredients = {}
+            for n in range(len(traitSelection)):
+                traitIngredients[n] = set(list(choosenIngredients.loc[choosenIngredients[traitSelection[n]] > 0,].index))
+            commonTraitIngredients = set.intersection(*traitIngredients.values())
+            commonTraitIngredients = list(commonTraitIngredients)
+
+
         for n in range(len(magiminUsed)):
             magiminValues[n] = choosenIngredients.iloc[:,magiminUsed[n]]
         ingredientsNumbers = [*range(len(choosenIngredients))]
-        if dailyIngredientLimit == "Yes":
+        if  (dailyIngredientLimit == "Yes") and (len(traitSelection) > 0):
+            combs = []
+            for item in itertools.combinations_with_replacement(ingredientsNumbers, ingredientNumber):
+                unique = list(set(item))
+                dailyLimit = False
+                for n in range(len(unique)):
+                    if item.count(unique[n]) > choosenIngredients.iloc[unique[n], 8]:
+                        dailyLimit = True
+                if dailyLimit == False:
+                    traitIngredientsCombi = []
+                    for n in range(len(traitSelection)):
+                        traitIngredientsCombi.append(any(ingredient in traitIngredients[n] for ingredient in unique))
+                    if (any(ingredient in commonTraitIngredients for ingredient in unique)) or (all(traitIngredientsCombi)):
+                        combs.append(list(item))
+        elif len(traitSelection) > 0:
+            combs = []
+            for item in itertools.combinations_with_replacement(ingredientsNumbers, ingredientNumber):
+                unique = list(set(item))
+                traitIngredientsCombi = []
+                for n in range(len(traitSelection)):
+                    traitIngredientsCombi.append(any(ingredient in traitIngredients[n] for ingredient in unique))
+                if (any(ingredient in commonTraitIngredients for ingredient in unique)) or (all(traitIngredientsCombi)):
+                    combs.append(list(item))
+        elif dailyIngredientLimit == "Yes":
             combs = []
             for item in itertools.combinations_with_replacement(ingredientsNumbers, ingredientNumber):
                 unique = list(set(item))
@@ -303,13 +456,19 @@ class MagiminsCalculator(QtWidgets.QDialog):
         self.dailyIngredientLimit = QComboBox()
         self.dailyIngredientLimit.addItem('Yes')
         self.dailyIngredientLimit.addItem('No')
+        self.traitSelection = CheckableComboBox()
+        self.traitSelection.addItem("Taste")
+        self.traitSelection.addItem("Sensation")
+        self.traitSelection.addItem("Aroma")
+        self.traitSelection.addItem("Visual")
+        self.traitSelection.addItem("Sound")
         self.calculateButton = QPushButton("Calculate")
         self.calculateButton.setDefault(True)
         self.totalMagimins = QLabel()
         self.totalMagimins.setText("Total Magimins: 0")
 
         hlay = QGridLayout(self)
-        hlay.addWidget(self.ingredientTable, 0, 0, 9, 1)
+        hlay.addWidget(self.ingredientTable, 0, 0, 10, 1)
         hlay.addWidget(ingredientNumberLabel, 0, 1)
         hlay.addWidget(self.ingredientNumber, 1, 1)
         hlay.addWidget(magiminsNumberLabel, 2, 1)
@@ -318,9 +477,10 @@ class MagiminsCalculator(QtWidgets.QDialog):
         hlay.addWidget(self.potionMaking, 5, 1)
         hlay.addWidget(dailyLabel, 6, 1)
         hlay.addWidget(self.dailyIngredientLimit, 7, 1)
-        hlay.addWidget(self.calculateButton, 8, 1)
+        hlay.addWidget(self.traitSelection, 8, 1)
+        hlay.addWidget(self.calculateButton, 9, 1)
         hlay.addWidget(self.totalMagimins, 0, 2)
-        hlay.addWidget(self.solutionTable, 1, 2, 8, 1)
+        hlay.addWidget(self.solutionTable, 1, 2, 9, 1)
 
     def closeEvent(self, event):
         excelLoc = './Potionomics.xlsx'
